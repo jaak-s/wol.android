63ee4bc14765517d5a98e3e2bed552c3c7fa79ef
Sven-Erik Petermann <sverik@gmail.com>
2014-08-26 23:22:07 +0300

    Cleanup. Remove dots.

--- WheelOfLife/src/org/po/wol/WolThread.java
+++ WheelOfLife/src/org/po/wol/WolThread.java
@@ -1,7 +1,5 @@
 package org.po.wol;
 
-import java.util.ArrayList;
-
 import org.po.wol.logic.Logic;
 import org.po.wol.state.InputState;
 import org.po.wol.state.State.Season;
@@ -12,7 +10,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Align;
-import android.graphics.Paint.Style;
 import android.graphics.PointF;
 import android.graphics.RectF;
 import android.view.SurfaceHolder;
@@ -20,11 +17,8 @@ import android.view.SurfaceHolder;
 public class WolThread extends Thread {
 	private int mCanvasWidth;
 	private int mCanvasHeight;
-	private ArrayList<dot> Dots = new ArrayList<dot>(); // Dynamic array with
-														// dots
 	private SurfaceHolder holder;
 	private boolean running = false;
-	private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 	private final int refresh_rate = 20; // How often we update the screen, in
 											// ms
 
@@ -45,7 +39,6 @@ public class WolThread extends Thread {
 
 	//private long delta;
 	private long seasonTextRemaining;
-	private int textWidth;
 
 	private Season previousSeason = null;
 	private InputState inputState;
@@ -66,23 +59,6 @@ public class WolThread extends Thread {
 		this.state = state;
 		this.inputState = inputState;
 		this.logic = logic;
-	}
-
-	public synchronized void addDot() {
-		int x, y, radius;
-		float[] color = new float[3]; // HSV (0..360,0..1,0..1)
-
-		// ADD ONE MORE DOT TO THE SCREEN
-		x = 100 + (int) (Math.random() * (mCanvasWidth - 200));
-		y = 100 + (int) (Math.random() * (mCanvasHeight - 200));
-		radius = 1 + (int) (Math.random() * 99);
-		color[0] = (float) (Math.random() * 360);
-		color[1] = 1;
-		color[2] = 1;
-		dot mdot = new dot(x, y, radius, Color.HSVToColor(128, color));
-		if (Dots.size() >= 6) Dots.remove(0);
-		Dots.add(mdot);
-
 	}
 
 	@Override
@@ -140,23 +116,9 @@ public class WolThread extends Thread {
 	
 	private void draw(Canvas canvas) {
 		paintBackground(canvas);
-		paintDots(canvas);
 		paintWorld(canvas);
 		paintBloke(canvas);
 		paintOverlay(canvas);
-	}
-
-	// The actual drawing in the Canvas (not the update to the screen).
-	private void paintDots(Canvas canvas) {
-		dot temp_dot;
-		paint.setStyle(Style.FILL_AND_STROKE);
-		for (int i = 0; i < Dots.size(); i++) {
-			temp_dot = Dots.get(i);
-			paint.setColor(temp_dot.get_color());
-			canvas.drawCircle((float) temp_dot.get_x(),
-					(float) temp_dot.get_y(), (float) temp_dot.get_radius(),
-					paint);
-		}
 	}
 
 	// /////////// sverik's code
@@ -301,33 +263,6 @@ public class WolThread extends Thread {
 			this.height = mCanvasHeight;
 			this.w_2 = width / 2;
 
-		}
-	}
-
-	private class dot {
-		private int x, y, radius, color;
-
-		dot(int x, int y, int radius, int color) {
-			this.x = x;
-			this.y = y;
-			this.radius = radius;
-			this.color = color;
-		}
-
-		public int get_x() {
-			return this.x;
-		}
-
-		public int get_y() {
-			return this.y;
-		}
-
-		public int get_radius() {
-			return this.radius;
-		}
-
-		public int get_color() {
-			return this.color;
 		}
 	}
 
